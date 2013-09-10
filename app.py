3d478a814577a2b4ec2fd9d391d13523ca114c09
RJackson <rob@rjackson.me>
2013-09-10 05:16:11 +0100

    Oh looky typo frmo copy paste skils

--- app.py
+++ app.py
@@ -66,6 +66,6 @@ else:
     credentials = None
     if app.config["MAIL_USERNAME"] or app.config["MAIL_PASSWORD"]:
         credentials = (app.config["MAIL_USERNAME"], app.config["MAIL_PASSWORD"])
-    mail_handler = SMTPHandler((app.config["MAIL_SERVER"], app.config["MAIL_PORT"]), 'no-reply@' + app.config["MAIL_SERVER"], app.config["ADMINS"], 'microblog failure', credentials)
+    mail_handler = SMTPHandler((app.config["MAIL_SERVER"], app.config["MAIL_PORT"]), 'no-reply@' + app.config["MAIL_SERVER"], app.config["ADMINS"], 'dotabank failure', credentials)
     mail_handler.setLevel(logging.ERROR)
     app.logger.addHandler(mail_handler)
