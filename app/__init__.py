81b27ec5b3c8818ebc55c12159bb6b2c13107130
RJackson <rob@rjackson.me>
2013-09-13 15:24:55 +0100

    Reinstate production/debug specific features

--- app/__init__.py
+++ app/__init__.py
@@ -39,28 +39,18 @@ admin.add_view(AdminModelView(ReplayFavourite, db.session, category='Replays'))
 admin.add_view(GCWorkerAdmin(db.session, category='GC'))
 admin.add_view(AdminModelView(GCJob, db.session, category='GC'))
 
+# Debug environment
+if app.debug:
+    from flask.ext.debugtoolbar import DebugToolbarExtension
+    toolbar = DebugToolbarExtension(app)
 
-# # Have the views' code get a ran through - they register themselves, hence why no actual importing.
-# from admin import admin
-# admin.init_app(app)
-#
-
-# # Debug environment
-# if app.debug:
-#     from flask.ext.debugtoolbar import DebugToolbarExtension
-#     toolbar = DebugToolbarExtension(app)
-#
-# # Production environment code
-# else:
-#     import logging
-#     from logging.handlers import SMTPHandler
-#     credentials = None
-#     if app.config["MAIL_USERNAME"] or app.config["MAIL_PASSWORD"]:
-#         credentials = (app.config["MAIL_USERNAME"], app.config["MAIL_PASSWORD"])
-#     mail_handler = SMTPHandler((app.config["MAIL_SERVER"], app.config["MAIL_PORT"]), 'no-reply@' + app.config["MAIL_SERVER"], app.config["ADMINS"], 'dotabank failure', credentials)
-#     mail_handler.setLevel(logging.ERROR)
-#     app.logger.addHandler(mail_handler)
-
-
-if __name__ == '__main__':
-    app.run()
+# Production environment code
+else:
+    import logging
+    from logging.handlers import SMTPHandler
+    credentials = None
+    if app.config["MAIL_USERNAME"] or app.config["MAIL_PASSWORD"]:
+        credentials = (app.config["MAIL_USERNAME"], app.config["MAIL_PASSWORD"])
+    mail_handler = SMTPHandler((app.config["MAIL_SERVER"], app.config["MAIL_PORT"]), 'no-reply@' + app.config["MAIL_SERVER"], app.config["ADMINS"], 'dotabank failure', credentials)
+    mail_handler.setLevel(logging.ERROR)
+    app.logger.addHandler(mail_handler)
