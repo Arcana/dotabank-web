78191accbd5c1ca572a422d80cbb6e193dec1f2d
RJackson <rob@rjackson.me>
2013-09-18 11:28:54 +0100

    Parser now saves player data snapshots

--- app/replays/models.py
+++ app/replays/models.py
@@ -122,3 +122,171 @@ class CombatLogMessage(db.Model):
         self.target_source_name = target_source_name
         self.value = value
         self.health = health
+
+
+class ReplayPlayer(db.Model):
+    __tablename__ = "replay_players"
+
+    id = db.Column(db.Integer, primary_key=True)
+    replay_id = db.Column(db.Integer, db.ForeignKey("replays.id", ondelete="CASCADE"), nullable=False)
+
+    # name = Column(String()) # Player name, we don't need to store this.
+    steam_id = db.Column(db.BigInteger)
+    team = db.Column(db.Enum("radiant", "dire", "spectator"))
+
+    player_snapshots = db.relationship('PlayerSnapshot', backref='player', lazy='joined', cascade="all, delete-orphan")
+
+    def __init__(self, replay_id, steam_id, team):
+        self.replay_id = replay_id
+        self.steam_id = steam_id
+        self.team = team
+
+
+class PlayerSnapshot(db.Model):
+    __tablename__ = "replay_player_snapshots"
+
+    id = db.Column(db.Integer, primary_key=True)
+    player_id = db.Column(db.Integer, db.ForeignKey("replay_players.id", ondelete="CASCADE"), nullable=False)
+
+    index = db.Column(db.Integer)
+    tick = db.Column(db.Integer)
+
+    kills = db.Column(db.Integer)
+    deaths = db.Column(db.Integer)
+    assists = db.Column(db.Integer)
+    streak = db.Column(db.Integer)
+
+    last_hits = db.Column(db.Integer)
+    denies = db.Column(db.Integer)
+    earned_gold = db.Column(db.Integer)
+    reliable_gold = db.Column(db.Integer)
+    unreliable_gold = db.Column(db.Integer)
+    total_gold = db.Column(db.Integer)
+
+    has_buyback = db.Column(db.Boolean)
+    last_buyback_time = db.Column(db.Integer)
+    buyback_cooldown_time = db.Column(db.Float)
+
+    # BaseNPC
+    # abilities  # Do we care?
+    level = db.Column(db.Integer)
+    life_state = db.Column(db.Enum(
+        "alive",
+        "dying"
+        "dead",
+        "respawnable",
+        "discardbody"
+    ))
+    is_alive = db.Column(db.Boolean)
+    health = db.Column(db.Integer)
+    health_regen = db.Column(db.Float)
+    max_health = db.Column(db.Integer)
+
+    mana = db.Column(db.Float)
+    mana_regen = db.Column(db.Float)
+    max_mana = db.Column(db.Float)
+
+    position_x = db.Column(db.Float)
+    position_y = db.Column(db.Float)
+
+    # Hero
+    hero_name = db.Column(db.String(42))
+
+    natural_agility = db.Column(db.Float)
+    natural_intelligence = db.Column(db.Float)
+    natural_strength = db.Column(db.Float)
+
+    agility = db.Column(db.Float)
+    intelligence = db.Column(db.Float)
+    strength = db.Column(db.Float)
+
+    ability_points = db.Column(db.Integer)
+
+    recent_damage = db.Column(db.Integer)
+    replicating_hero = db.Column(db.Boolean)
+    respawn_time = db.Column(db.Float)
+    spawned_at = db.Column(db.Float)
+    xp = db.Column(db.Integer)
+
+    def __init__(self,
+                 player_id,
+                 tick,
+                 index,
+                 kills,
+                 deaths,
+                 assists,
+                 streak,
+                 last_hits,
+                 denies,
+                 earned_gold,
+                 reliable_gold,
+                 unreliable_gold,
+                 total_gold,
+                 has_buyback,
+                 last_buyback_time,
+                 buyback_cooldown_time,
+                 level=None,
+                 life_state=None,
+                 is_alive=None,
+                 health=None,
+                 health_regen=None,
+                 max_health=None,
+                 mana=None,
+                 mana_regen=None,
+                 max_mana=None,
+                 position_x=None,
+                 position_y=None,
+                 hero_name=None,
+                 natural_agility=None,
+                 natural_intelligence=None,
+                 natural_strength=None,
+                 agility=None,
+                 intelligence=None,
+                 strength=None,
+                 ability_points=None,
+                 recent_damage=None,
+                 replicating_hero=None,
+                 respawn_time=None,
+                 spawned_at=None,
+                 xp=None
+                 ):
+        self.player_id = player_id
+        self.tick = tick
+        self.index = index
+        self.kills = kills
+        self.deaths = deaths
+        self.assists = assists
+        self.streak = streak
+        self.last_hits = last_hits
+        self.denies = denies
+        self.earned_gold = earned_gold
+        self.reliable_gold = reliable_gold
+        self.unreliable_gold = unreliable_gold
+        self.total_gold = total_gold
+        self.has_buyback = has_buyback
+        self.last_buyback_time = last_buyback_time
+        self.buyback_cooldown_time = buyback_cooldown_time
+        self.level = level
+        self.life_state = life_state
+        self.is_alive = is_alive
+        self.health = health
+        self.health_regen = health_regen
+        self.max_health = max_health
+        self.mana = mana
+        self.mana_regen = mana_regen
+        self.max_mana = max_mana
+        self.position_x = position_x
+        self.position_y = position_y
+        self.hero_name = hero_name
+        self.natural_agility = natural_agility
+        self.natural_intelligence = natural_intelligence
+        self.natural_strength = natural_strength
+        self.agility = agility
+        self.intelligence = intelligence
+        self.strength = strength
+        self.ability_points = ability_points
+        self.recent_damage = recent_damage
+        self.replicating_hero = replicating_hero
+        self.respawn_time = respawn_time
+        self.spawned_at = spawned_at
+        self.xp = xp
