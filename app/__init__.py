5836e613eea4fbbe06290dfca34c58a4661c9448
RJackson <rob@rjackson.me>
2014-02-11 10:26:02 +0000

    Better method of splitting players into teams.  Also fixes an IndexError.

--- app/__init__.py
+++ app/__init__.py
@@ -50,7 +50,8 @@ from filters import escape_every_character,\
     dotabuff_match_link,\
     lobby_type,\
     game_mode,\
-    building_status
+    building_status,\
+    players_to_teams
 
 app.add_template_filter(escape_every_character)
 app.add_template_filter(get_steamid_from_accountid)
@@ -66,6 +67,7 @@ app.add_template_filter(dotabuff_match_link)
 app.add_template_filter(lobby_type)
 app.add_template_filter(game_mode)
 app.add_template_filter(building_status)
+app.add_template_filter(players_to_teams)
 
 from views import index, about, internalerror
 
