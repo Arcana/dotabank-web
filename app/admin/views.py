d4503f5e5099b47994185b1385baf3f7b2d8b431
RJackson <rob@rjackson.me>
2014-02-11 11:32:33 +0000

    Add a reports page to the admin section to flag up potentially mis-imported data, we can expand and improve this over time.

--- app/admin/views.py
+++ app/admin/views.py
@@ -1,11 +1,14 @@
 from flask import Blueprint, g, current_app
-from flask.ext.admin import Admin, expose, AdminIndexView
+from flask.ext.admin import Admin, expose, AdminIndexView, BaseView
 from flask.ext.admin.contrib.sqlamodel import ModelView
 from flask.ext.login import current_user
 
+from sqlalchemy.sql import text
 from datetime import datetime, timedelta
 
+from app import db
 from app.gc.models import GCJob, GCWorker
+from app.replays.models import Replay, ReplayPlayer
 
 mod = Blueprint("dotabank_admin", __name__)
 
@@ -49,7 +52,32 @@ class AdminIndex(AuthMixin, AdminIndexView):
                            stats=stats)
 
 
+class Reports(BaseView):
+    @expose('/')
+    def index(self):
+        replays_without_ten_players = [x for x in db.engine.execute(
+            text("""
+                SELECT
+                    r.id,
+                    count(*) as player_count
+                FROM {replay_table} r
+                LEFT JOIN {player_table} rp ON rp.replay_id = r.id
+                WHERE
+                    r.game_mode NOT IN (7, 9) # Disgard diretide (7)/greeviling (9)
+                GROUP BY rp.replay_id
+            """.format(
+                replay_table=Replay.__tablename__,
+                player_table=ReplayPlayer.__tablename__)
+            )
+        ) if x.player_count != 10]  # Do the "not-10" check in the app cause idfk how to do it in sql.
+
+        return self.render(
+            'admin/reports.html',
+            replays_without_ten_players=replays_without_ten_players
+        )
+
 admin = Admin(name="Dotabank", index_view=AdminIndex())
+admin.add_view(Reports(name='Reports'))
 
 
 @mod.before_app_request
