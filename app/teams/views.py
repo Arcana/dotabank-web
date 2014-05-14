e93189c47020fb8ba032e0b1a08a07bf8bdf7a3d
RJacksonm1 <rob@rjackson.me>
2014-05-14 11:54:52 +0000

    Fixing IndexError in teams/team/<id> by replacing the entire loop with something much simpler.

--- app/teams/views.py
+++ app/teams/views.py
@@ -11,15 +11,12 @@ def team(_id, page=1):
     _replays = Replay.query.filter(or_(Replay.radiant_team_id == _id, Replay.dire_team_id == _id)).order_by(Replay.id.desc()).paginate(page, current_app.config["REPLAYS_PER_PAGE"])
     if _replays.total <= 0:
         abort(404)
-    team_found = False
-    loop_num = 0
-    while not team_found:
-        if _replays.items[loop_num].radiant_team_id == _id:
-            _team = {'id': _id,
-                     'name': _replays.items[loop_num].radiant_team_name,
-                     'logo': _replays.items[loop_num].radiant_team_logo}
-            team_found = True
-        loop_num += 1
+        
+    _team = {
+        'id': _id,
+        'name': _replays.items[0].radiant_team_name if _replays.items[0].radiant_team_id == _id else _replays.items[0].dire_team_name,
+        'logo': _replays.items[0].radiant_team_logo if _replays.items[0].radiant_team_id == _id else _replays.items[0].dire_team_logo
+    }
 
     return render_template("teams/team.html",
                            title="{} replays - Dotabank".format(_team['name']),
