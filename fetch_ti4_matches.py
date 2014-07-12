7423e810c92d2578c101bb5f3f16d92e1f3eda9b
RJackson <rob@rjackson.me>
2014-07-12 12:08:30 +0100

    Refactored TI4 match fetch script to be more generic and functional.  Also added support for paginating through GetMatchHistory results.

--- fetch_ti4_matches.py
+++ fetch_ti4_matches.py
@@ -8,19 +8,53 @@ from app.replays.models import Replay, ReplayPlayer
 
 THE_INTERNATIONAL_4_ID = 600
 
-matches = steam.api.interface("IDOTA2Match_570").GetMatchHistory(league_id=THE_INTERNATIONAL_4_ID).get("result")
 
-if matches:
-    for match in matches.get("matches"):
-        replay_exists = Replay.query.filter(Replay.id == match["match_id"]).count() > 0
+def get_league_matches(league_id, all_pages=True):
+    """
+    Queries the WebAPI and fetches the list of matches for the given league ID.
 
-        if not replay_exists:
-            replay = Replay(match["match_id"])
-            db.session.add(replay)
-            db.session.commit()
+    @param league_id: The league ID for which we want to find matches.
+    @param all_pages: Whether or not to paginate through the results and return every match, or just the latest 100.
+    """
+    matches = []  # List of matches
+    match_query = None  # Results of WebAPI query.
 
-            Replay.add_gc_job(replay)
-
-            print "Added {} to database and job queue".format(match["match_id"])
+    while match_query is None or (match_query.get('results_remaining') > 0 and all_pages is True):
+        # If we already have some matches, use the last one's match id as the beginning of our query.
+        if len(matches) > 0:
+            match_query = steam.api.interface("IDOTA2Match_570").GetMatchHistory(league_id=league_id, start_at_match_id=matches[-1].get("match_id")).get("result")
+        # Otherwise get whatever
         else:
-            print "Match {} already in database, skipping.".format(match["match_id"])
+            match_query = steam.api.interface("IDOTA2Match_570").GetMatchHistory(league_id=league_id).get("result")
+
+        # Add the new matches to our list of matches
+        matches.extend(match_query.get('matches'))
+
+    return matches
+
+
+def process_match_list(matches):
+    """ Iterates through a list ofmatches and checks whether we already have them in our database. If we do not then
+    this code will add the match to our database and create an associated GC job. """
+    if len(matches) > 0:
+        for match in matches:
+            replay_exists = Replay.query.filter(Replay.id == match["match_id"]).count() > 0
+
+            if not replay_exists:
+                replay = Replay(match["match_id"])
+                db.session.add(replay)
+                db.session.commit()
+
+                Replay.add_gc_job(replay)
+
+                print "Added {} to database and job queue".format(match["match_id"])
+            else:
+                print "Match {} already in database, skipping.".format(match["match_id"])
+
+
+def main():
+    matches = get_league_matches(THE_INTERNATIONAL_4_ID)
+    process_match_list(matches)
+
+if __name__ == "__main__":
+    main()
